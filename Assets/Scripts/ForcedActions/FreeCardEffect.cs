53815772db5e9029d875b8dc96d9bad9bc79136f
lucohen <lucohen@chapman.edu>
2026-06-12 12:53:18 -0700

    Checkpoint

--- Assets/Scripts/ForcedActions/FreeCardEffect.cs
+++ Assets/Scripts/ForcedActions/FreeCardEffect.cs
@@ -8,7 +8,7 @@ public class FreeCardAction : PendingAction
     private readonly int _amount;
     private readonly bool _addToHand;
 
-    public override string Prompt => $"Discard {_amount} card(s).";
+    public override string Prompt => "Choose a card to purchase.";
 
     public FreeCardAction(GalaxyShop shop, int amount, bool addToHand)
     {
@@ -26,7 +26,8 @@ public class FreeCardAction : PendingAction
         if (target is Card card && card.MatchesPlayerFaction(player)){
             CardGame.Instance.galaxyShop.nextCardFree = true;
             if (_addToHand) CardGame.Instance.galaxyShop.addNextCardToHand = true;
-            player.PurchaseCard(card);
+            ManagePurchases.Instance.PurchaseCard(card, player);
+            ActionManager.Instance.Resolve();
         }
     }
 
@@ -34,6 +35,30 @@ public class FreeCardAction : PendingAction
         => Messenger.Instance.ShowPrompt(Prompt);
 
     public override void OnResolved()
-        => Messenger.Instance.HidePrompt();
+    {
+        
+        Messenger.Instance.HidePrompt();
+    }
 }
 
+[CreateAssetMenu(menuName = "Effects/FreeCardEffect")]
+public class FreeCardEffect : CardEffect                //Card effect that pushes the action onto the stack
+
+{
+    public override void Resolve(EffectData data)
+    {
+
+        var freeCardData = (FreeCardData)data;
+        
+        FreeCardAction action = new FreeCardAction(CardGame.Instance.galaxyShop, freeCardData.amount, freeCardData.addToHand);
+
+        ActionManager.Instance.Push(action);
+
+    }
+}
+
+public class FreeCardData : EffectData
+{
+    public bool addToHand;
+    public int amount;
+}
