786b3bc05c762d9b2dc3c66a7b16ba35110551b2
lucohen <lucohen@chapman.edu>
2026-06-12 18:36:18 -0700

    Revert "Checkpoint"
    This reverts commit 53815772db5e9029d875b8dc96d9bad9bc79136f.

--- Assets/Scripts/ForcedActions/FreeCardAction.cs
+++ Assets/Scripts/ForcedActions/FreeCardAction.cs
@@ -8,7 +8,7 @@ public class FreeCardAction : PendingAction
     private readonly int _amount;
     private readonly bool _addToHand;
 
-    public override string Prompt => "Choose a card to purchase.";
+    public override string Prompt => $"Discard {_amount} card(s).";
 
     public FreeCardAction(GalaxyShop shop, int amount, bool addToHand)
     {
@@ -26,8 +26,7 @@ public class FreeCardAction : PendingAction
         if (target is Card card && card.MatchesPlayerFaction(player)){
             CardGame.Instance.galaxyShop.nextCardFree = true;
             if (_addToHand) CardGame.Instance.galaxyShop.addNextCardToHand = true;
-            ManagePurchases.Instance.PurchaseCard(card, player);
-            ActionManager.Instance.Resolve();
+            player.PurchaseCard(card);
         }
     }
 
@@ -35,30 +34,6 @@ public class FreeCardAction : PendingAction
         => Messenger.Instance.ShowPrompt(Prompt);
 
     public override void OnResolved()
-    {
-        
-        Messenger.Instance.HidePrompt();
-    }
+        => Messenger.Instance.HidePrompt();
 }
 
-[CreateAssetMenu(menuName = "Effects/FreeCardEffect")]
-public class FreeCardEffect : CardEffect                //Card effect that pushes the action onto the stack
-
-{
-    public override void Resolve(EffectData data)
-    {
-
-        var freeCardData = (FreeCardData)data;
-        
-        FreeCardAction action = new FreeCardAction(CardGame.Instance.galaxyShop, freeCardData.amount, freeCardData.addToHand);
-
-        ActionManager.Instance.Push(action);
-
-    }
-}
-
-public class FreeCardData : EffectData
-{
-    public bool addToHand;
-    public int amount;
-}
