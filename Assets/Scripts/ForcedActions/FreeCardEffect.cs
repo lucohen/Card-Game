fb2bb855a748eac4cd5fcd83c61738cfa1050e47
lucohen <lucohen@chapman.edu>
2026-06-12 18:51:09 -0700

    stop

--- Assets/Scripts/ForcedActions/FreeCardEffect.cs
+++ Assets/Scripts/ForcedActions/FreeCardEffect.cs
@@ -8,7 +8,11 @@ public class FreeCardAction : PendingAction
     private readonly int _amount;
     private readonly bool _addToHand;
 
+<<<<<<< HEAD
     public override string Prompt => $"Discard {_amount} card(s).";
+=======
+    public override string Prompt => "Choose a card to purchase.";
+>>>>>>> parent of 624a96e (Revert "Checkpoint")
 
     public FreeCardAction(GalaxyShop shop, int amount, bool addToHand)
     {
@@ -26,7 +30,12 @@ public class FreeCardAction : PendingAction
         if (target is Card card && card.MatchesPlayerFaction(player)){
             CardGame.Instance.galaxyShop.nextCardFree = true;
             if (_addToHand) CardGame.Instance.galaxyShop.addNextCardToHand = true;
+<<<<<<< HEAD
             player.PurchaseCard(card);
+=======
+            ManagePurchases.Instance.PurchaseCard(card, player);
+            ActionManager.Instance.Resolve();
+>>>>>>> parent of 624a96e (Revert "Checkpoint")
         }
     }
 
@@ -34,6 +43,36 @@ public class FreeCardAction : PendingAction
         => Messenger.Instance.ShowPrompt(Prompt);
 
     public override void OnResolved()
+<<<<<<< HEAD
         => Messenger.Instance.HidePrompt();
 }
 
+=======
+    {
+        
+        Messenger.Instance.HidePrompt();
+    }
+}
+
+[CreateAssetMenu(menuName = "Effects/FreeCardEffect")]
+public class FreeCardEffect : CardEffect                //Card effect that pushes the action onto the stack
+
+{
+    public override void Resolve(EffectData data)
+    {
+
+        var freeCardData = (FreeCardData)data;
+        
+        FreeCardAction action = new FreeCardAction(CardGame.Instance.galaxyShop, freeCardData.amount, freeCardData.addToHand);
+
+        ActionManager.Instance.Push(action);
+
+    }
+}
+
+public class FreeCardData : EffectData
+{
+    public bool addToHand;
+    public int amount;
+}
+>>>>>>> parent of 624a96e (Revert "Checkpoint")
